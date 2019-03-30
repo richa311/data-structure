1ee93178fb2a542032be2e97579830e23a765084
Richa <richa.rochna789@gmail.com>
2019-03-30 19:02:43 +0530

    Add files via upload

---
+++ src/array/basic/MissingElement.java
@@ -0,0 +1,19 @@
+package array.basic;
+
+public class MissingElement {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,6};
+		System.out.println(missingNumber(arr));
+	}
+	
+	public static int missingNumber(int arr[]) {
+		for(int i=0;i<arr.length;i++) {
+			int miss = ++arr[i];
+			if(arr[i+1] != miss)
+				return miss;
+			
+		}
+		return 0;
+	}
+}
---
+++ src/stacks/Compiler.java
@@ -0,0 +1,37 @@
+package stacks;
+
+import java.util.Stack;
+
+public class Compiler {
+
+	public static void main(String[] args) {
+		String s = "{[()}]";
+		System.out.println(compile(s));
+
+	}
+	
+	public static boolean compile(String s) {
+		int l = s.length();
+		boolean flag = true;
+		Stack<Character> stack = new Stack<>();
+		for(int i = 0;i<l;i++) {
+			char c = s.charAt(i);
+			switch(c) {
+			case '{':
+				stack.push('}');
+				break;
+			case '[':
+				stack.push(']');
+				break;
+			case '(':
+				stack.push(')');
+				break;
+			default:
+				if(c != stack.pop())
+					flag = false;
+				break;
+			}
+		}
+		return flag;
+	}
+}
